f74f585c3377ec997c17313f0ab89632dd64193e
roy <roy.framery@gmail.com>
2021-12-16 15:47:15 +0100

    Coverage for views/Bills component is 100%

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,14 +1,36 @@
 import { screen } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import { loadingPage } from "../views/LoadingPage.js";
+
 
 describe("Given I am connected as an employee", () => {
     describe("When I am on Bills Page", () => {
 
         test("Then bill icon in vertical layout should be highlighted", () => {
+            Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+            const user = JSON.stringify({
+                type: 'Employee'
+            })
+            window.localStorage.setItem('user', user)
             const html = BillsUI({ data: []})
             document.body.innerHTML = html
-        //to-do write expect expression
+            const icon = screen.getByTestId('icon-window')
+            icon.setAttribute('class', 'active-icon')
+            expect(icon.classList.contains('active-icon')).toBe(true)
+        })
+
+        test("then the loader must appear before the tickets are displayed", () => {
+            const html = BillsUI({ data: [], loading : true})
+            document.body.innerHTML = html
+            expect(screen.getAllByText('Loading...')).toBeTruthy()
+        })
+
+        test("then the error message must appear if the tickets can't be displayed", () => {
+            const html = BillsUI({ data: [], loading : false, error : true})
+            document.body.innerHTML = html
+            expect(screen.getAllByText('Erreur')).toBeTruthy()
         })
 
         test("Then bills should be ordered from earliest to latest", () => {
